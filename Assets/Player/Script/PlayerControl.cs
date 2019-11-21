cb8bd95cf7bcd8b31b53d4a23d9a9db3d37baf76
isocele <hugo.chollet@epitech.eu>
2019-11-21 15:46:09 +0900

    FPS camera and shoot working

---
+++ Assets/Player/Script/PlayerControl.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 40;
+    private Rigidbody rigidBody;
+    private float x_min;
+    private float x_max;
+    private float z_min;
+    private float z_max;
+
+    private bool onGround;
+
+    public float mouseSpeed = 4.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        KeyboardMovement();
+        JumpEvent();
+        MoveCamera();
+    }
+
+    private void KeyboardMovement()
+    {
+        float xMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float zMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float xSpeed = xMove * speed;
+        float zSpeed = zMove * speed;
+        Vector3 newVelocity = new Vector3(xSpeed, rigidBody.velocity.y, zSpeed);
+        rigidBody.velocity = newVelocity;
+    }
+
+    private void JumpEvent()
+    {
+        if (Input.GetButtonDown("Jump") && onGround)
+        {
+            onGround = false;
+            Vector3 newVelocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y + 3, rigidBody.velocity.z);
+            rigidBody.velocity = newVelocity;
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            print("Hit");
+        }
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            print("t bon");
+            onGround = true;
+        }
+    }
+
+    void MoveCamera()
+    {
+        yaw += mouseSpeed * Input.GetAxis("Mouse X");
+        pitch -= mouseSpeed * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
---
+++ Assets/Player/Script/WeaponManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public GameObject projectile;
+    private float fireRate = 2;
+    private float nextFire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire(transform.forward, transform.position);
+        }
+    }
+
+    public void Fire(Vector3 direction, Vector3 spawnPos)
+    {
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            GameObject go = Instantiate(projectile, spawnPos, Quaternion.identity);
+            go.GetComponent<Projectile>().setDirection(direction);
+        }
+    }
+
+}
